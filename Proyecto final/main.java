041059ba2d18f676e0a04ebee3906cd55341735d
Santi160206 <107377949+Santi160206@users.noreply.github.com>
2024-05-31 13:23:59 -0500

    variables

--- Proyecto final/main.java
+++ Proyecto final/main.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 public class main{
     public static void main(String[] args) {
         Scanner sc1 = new Scanner(System.in);
-        int Contraseña, Id, op;
+      Libreria lib= new Libreria();
+        
         System.out.println(" Bienvenido a la libreria libros y revistas ");
         System.out.println("seleccione: 1)Propietario  2)Usuario   3)Editorial");
-        Id = sc1.nextInt();
+        lib.Id = sc1.nextInt();
         int intentos = 0;
-        if (Id == 1) {
+        if (lib.Id == 1) {
             do {
                 System.out.println("Ingrese contraseña");
-                Contraseña = sc1.nextInt();
+                lib.Contraseña = sc1.nextInt();
                 intentos++;
-            } while (Contraseña != 1234 && intentos <= 3);
+            } while (lib.Contraseña != 1234 && intentos <= 3);
             if (intentos <= 3) {
                 System.out.println("Bienvenido jefe");
                 System.out.println("Que desea observar \n1)Inventario ordenado alfabeticamente \n2)Listado de ventas");
-                op = sc1.nextInt();
-                if (op == 1) {
+                lib.op = sc1.nextInt();
+                if (lib.op == 1) {
                   lib.AbastecimientoLibros();
 
                 } else {
-                    if (op == 2) {
+                    if (lib.op == 2) {
                          //
                         }
                     }
@@ -30,34 +31,35 @@ public class main{
                 System.out.println("El numero de intentos fue excedido");
                 System.exit(0);
             }
-        } else if (Id == 2) {
+        } else if (lib.Id == 2) {
              
                 System.out.println("Bienvenido usuario");
                 System.out.println("como desea realizar su busqueda?");
                 System.out.println("1)Informacion Bibliografica respecto a un libro  2)Busqueda por autor   3)Busqueda por letra");
-                op = sc1.nextInt();
-                if (op == 1) {
+                lib.op = sc1.nextInt();
+                if (lib.op == 1) {
 
                 } else {
-                    if (op == 2) {
+                    if (lib.op == 2) {
+                     
 
-                    } else if (op == 3) {
-                         
+                    } else if (lib.op == 3) {
+                         System.out.println("");
 
                         }
                     }
             
           
-        }else if (Id==3){
+        }else if (lib.Id==3){
           
             System.out.println("Bienvenido");
               System.out.println("Desea ingresar Libros?");
               System.out.println("1) si 2)No");
-              op= sc1.nextInt();
-            if(op==1){
+              lib.op= sc1.nextInt();
+            if(lib.op==1){
               
             }else{
-              if(op==2)
+              if(lib.op==2)
                 System.out.println("Muchas gracias,Hasta pronto");
                   System.exit(0);
             } 

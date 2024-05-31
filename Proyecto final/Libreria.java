06789733fef109b22df226c6a2023307e9bd473b
Santi160206 <107377949+Santi160206@users.noreply.github.com>
2024-05-31 13:43:26 -0500

    Nuevos metodos

--- Proyecto final/Libreria.java
+++ Proyecto final/Libreria.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 public class Libreria {
+
+    int i;
     int Contraseña, Id, op;
+    String Busqueda;
+    
     static String[]libreria=new String[10];
     Scanner sc2=new Scanner(System.in);
 
     public void AbastecimientoLibros(){
-        int libros, cantidad=0,i;
+        int libros, cantidad=0;
 
         System.out.println("Ingrese la cantidad de libros");
         libros=sc2.nextInt();
@@ -16,6 +20,25 @@ public class Libreria {
         libreria[i]=sc2.next();
         cantidad+=libros;
        }
+    }
+
+    public void BusquedaLibrosLetra(){
+        do {
+            
+        } while (op==2);
+    }
+
+    public void BusquedaLibrosNombre(){
+        do {
+            for(i=0;i<libreria.length;i++){
+                if(Busqueda==libreria[i]){
+                    System.out.println(libreria[i]);
+                    
+                }
+
+            }
+            
+        } while (op==3);
     }
     
     public void LibrosAgotados(){}

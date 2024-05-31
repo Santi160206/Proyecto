ab61fc770ef2a13ea8a4ffbc23cdb70569f65a2f
SebastianUrrego19 <sebastianalvarez327216@correo.itm.edu.co>
2024-05-31 10:55:32 -0500

    Update main.java
    Edicion del main

--- Proyecto final/main.java
+++ Proyecto final/main.java
@@ -1,114 +1,62 @@
 import java.util.Scanner;
-
+  import static java.lang.System.exit;
 public class main{
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        Libreria lib= new Libreria();
-
-
-        //Variables
-        //String Titulo, Autor, Apellido, Precio, Editorial, ISBN, CopyRight, Disponibles, Estado;
-        int vector,c=0,i,j,libros=0;
-        int cantidad=0,Id,servicio,password;
-        String imp,sis="no",hay="si";
-
-        
-        
-
-    
-        System.out.println("Bienvenido a Librerias libros y revistas");
-
-        for(int p=0; p<1;){
-        System.out.println("Seleccione: 1)Propietario  2)Usuario   3)Editorial");
-        Id=sc.nextInt();
-
-        if(Id==1){
-            
+        Scanner sc1 = new Scanner(System.in);
+        int Contraseña, Id, op;
+        System.out.println(" Bienvenido a la libreria libros y revistas ");
+        System.out.println("seleccione: 1)Propietario  2)Usuario   3)Editorial");
+        Id = sc1.nextInt();
+        int intentos = 0;
+        if (Id == 1) {
             do {
                 System.out.println("Ingrese contraseña");
-            password=sc.nextInt();
-            c++;
+                Contraseña = sc1.nextInt();
+                intentos++;
+            } while (Contraseña != 1234 && intentos <= 3);
+            if (intentos <= 3) {
+                System.out.println("Bienvenido jefe");
+                System.out.println("Que desea observar \n1)Inventario ordenado alfabeticamente \n2)Depocitar más libros al Inventario   \n3)Listado de ventas");
+                op = sc1.nextInt();
+                if (op == 1) {
 
-            if(password==1234){
-                System.out.println("Bienvendio al sistema?");
-                System.out.println("¿Desea la impresion del inventario?");
-                imp=sc.next();
-                System.out.println("");
-                if(imp.equals("si")){
-                    
-                for(i=0; i<9; i++){
-                    for(j=0;j<lib.libreria.length;j++){
-                System.out.print(lib.libreria[i][j]+"\t");
-                
-            }
-            System.out.println();
-            }
-        }
-            }else{
-                System.out.println("Contraseña incorrecta");
-        }
-                
-            } while (password!=1234 && c<=12);
-           
-            
+                } else {
+                    if (op == 2) {
 
-     } else if(Id==2){
-            System.out.println("Bienvenido, ingrese el tipo de servicio:");
-            System.out.println("1)Busqueda de libro");
-            servicio=sc.nextInt();
+                    } else {
+                        if (op == 3) {
 
-        }else if(Id==3){
-            System.out.println("Bienvenido, ingrese el tipo de servicio:");
-            System.out.println("1)Abastecimiento");
-            servicio=sc.nextInt();
-            
-            while (hay.equals("si")) {
-                System.out.println("Cuantos libros desea ingersar");
-                cantidad=sc.nextInt();
-
-                
-                    for(j=0;j<cantidad;j++){
-                    System.out.print("Ingrese titular del libro");
-                    lib.libreria[0][j]=sc.next();
-                    System.out.print("\n Ingrese Autor del libro");
-                    lib.libreria[1][j]=sc.next();
-                    System.out.print("\nIngrese Apellido del libro");
-                    lib.libreria[2][j]=sc.next();
-                    System.out.print("\nIngrese Precio del libro");
-                    lib.libreria[3][j]=sc.next();
-                    System.out.print("\nIngrese Editorial del libro");
-                    lib.libreria[4][j]=sc.next();
-                    System.out.println("\nIngrese ISBN del libro");
-                    lib.libreria[5][j]=sc.next();
-                    System.out.print("\nIngrese CopyRight del libro");
-                    lib.libreria[6][j]=sc.next();
-                    System.out.print("\nIngrese Disponibles del libro");
-                    lib.libreria[7][j]=sc.next();
-                    System.out.print("\nIngrese Estado del libro");
-                    lib.libreria[8][j]=sc.next();
-                
+                        }
+                    }
                 }
+            } else {
+                System.out.println("El numero de intentos fue excedido");
+                System.exit(0);
+            }
+        } else {
+            if (Id == 2) {
+                System.out.println("Bienvenido usuario");
+                System.out.println("como desea realizar su busqueda?");
+                System.out.println("1)Informacion Bibliografica respecto a un libro  2)Busqueda por autor   3)Busqueda por letra");
+                op = sc1.nextInt();
+                if (op == 1) {
 
-                System.out.println("¿Desea ingresar mas libros?");
-                hay = sc.next();
+                } else {
+                    if (op == 2) {
 
-                libros+=cantidad;
-                System.out.println("La cantidad delibros es de:"+libros);
+                    } else {
+                        if (op == 3) {
+
+                        }
+                    }
+                }
+            }
         }
-             //hay="si"; El valor final debe ser positivo, porque si es negativo no entra al ciclo
-           
-
-        }else{
-            System.out.println("Opcion no valida");
-
-        }
-
-        
     }
 
+}
 
-
+    
 
 
 
@@ -116,5 +64,5 @@ public class main{
 
 
     
-}
-}
+
+

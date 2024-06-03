18d1b551e067cf310b1622395eb990c406f518f5
SebastianUrrego19 <sebastianalvarez327216@correo.itm.edu.co>
2024-06-02 23:54:21 -0500

    Update Libreria.java

--- Proyecto final/Libreria.java
+++ Proyecto final/Libreria.java
@@ -1,197 +1,208 @@
+package proyecto;
+
+
 import java.util.Scanner;
 
-public class Libreria {
-
-    Inventario inv= new Inventario();
-
-    int i,j,PPrecio, cont,PUnidInv;
-    String PNombre,PApellido,PTitulo,PISBN,PEditorial,PFechaCo;
- 
-    
-    static String[]libreria=new String[10];
-    Scanner sc2=new Scanner(System.in);
-
-  public void ventaLibros(){
-    String Respuc= "S";
-    int PCantic;
-    while("S".equals(Respuc)){
-        System.out.println("Ingrese el titulo del libro");
-        PTitulo= sc2.next();
-        System.out.println("Ingrese el nombre del autor");
-        PNombre=sc2.next();
-        System.out.println("Ingrese el apellido del autor");
-        PApellido=sc2.next();
-        System.out.println("Ingrese la cantidad de libros que desea comprar");
-        PCantic=sc2.nextInt();
-        int SWT=0, SWN=0, SWA=0;
-        for (int i = 0; i < 100; i++) {
-            if (inv.Autor[i].equals(PNombre)) {
-                SWN=0;
-                if (inv.Apellido[i].equals(PApellido)) {
-                    SWA=0;
-                    if (libreria[i].equals(PTitulo)) {
-                        SWT=0;
-                        if (inv.Ejemplares[i]>=PCantic) {
-                            inv.Ejemplares[i]-=PCantic;
-                            if (inv.Ejemplares[i]==0) {
-                                inv.Estado[i]=0;
-                                System.out.println("Compra exitosa");
-                                break;
-                                }
-                           }else{
-                                System.out.println("Solo contamos con "+ inv.Ejemplares[i]+"Uniadades para la venta");
-                                System.out.println("Desea comprarlas?(S/N)");
-                                String Resp= sc2.next();
-                                if ("S".equals(Resp)) {
-                                    inv.Ejemplares[i]=0;
-                                    inv.Estado[i]=0;
-                                    }
-                                }
-                        }else{
-                            SWT=1;
-                            }
-                    }else{
-                        SWA=1;
-                        }
-               }else{ 
-                    SWN=1;
-                    }
-           }  
-        if (SWT==1||SWA==1||SWN==1){
-            System.out.println("No se encuentra el libro deseado");
-            }
-        System.out.println("Desea comprar otro libro?(S/N)");
-        Respuc=sc2.next().toUpperCase();
-       }
-}
-
-    public void ordenAlfabeticamente(){
-        String aux;
-        
-                  for(i=0;i<libreria.length;i++){
-                    for(j=i+1;j<libreria.length;j++){
-                        if(libreria[i].compareTo(libreria[j])>0){
-                            aux=libreria[i];
-                            libreria[i]=libreria[j];
-                            libreria[j]=aux;
-      
-                        }
-                    }
-                }
-                for(i=0;i<libreria.length;i++){
-                System.out.println(libreria[i]);
-    }
-}
-
-
-
-    public void validacionVectorLLeno(){
-        int pos=0;
-        int libs;
+public class LIbros {
+    Scanner sc2= new Scanner(System.in);
+   Inventario inv= new Inventario();
+   String PNombre;
+   String PApellido;
+   String PTitulo;
+   String PISBN;
+   int PPrecio, cont;
+   String PEditorial;
+   String PFechaCo;
+   int PUnidInv;
+int pos=0;
+   public int validacionVectorLLeno(){
        for (int i = 0; i < 100; i++) {
            if ("".equals(inv.Autor[i])) {
                pos=i;
                break;
-           } else {
            }
          }
-           cont=pos;
-           String Respu="s";
-        while("s".equals(Respu)){
+        return pos;
+    }
 
-            System.out.println("Cuantos libros desea ingresar?");
-            libs=sc2.nextInt();
-
-            for(i=0;i<libs;i++){
-                System.out.println();
-
-             System.out.println("Digite el Titulo del libro");
-             libreria[i]= sc2.nextLine();
-             System.out.println("Digite el Nombre del autor");
-             PNombre= sc2.next();
-             System.out.println("Digite el Apellido del autor");
-             PApellido= sc2.next();
-             System.out.println("Digite el ISBN");
-             PISBN= sc2.next();
-             System.out.println("Digite la Editorial");
-             PEditorial= sc2.next();
-             System.out.println("Digite la Cantidad de Copias");
-             PUnidInv= sc2.nextInt();
-             System.out.println("Digite la Fecha de Copiright");
-             System.out.println("Ingrese el dia");
-             int dia= sc2.nextInt();
-             System.out.println("Ingrese el mes");
-             int mes= sc2.nextInt();
-             System.out.println("Ingrese el año");
-             int año=sc2.nextInt();
-                
-             /*if(año>=1800 && año<=2024){
-                 if (mes>=1 && mes<=12) {
-                        if (mes==2) {
-                         if (dia>0 && dia>=1 && dia<=28) {
-                             System.out.println("La fecha ingresada es: "+dia+"/"+mes+"/"+año);
-                             String Dia = Integer.toString(dia);
-                             String Mes = Integer.toString(mes);
-                             String Año= Integer.toString(año);
-                             PFechaCo= Dia+Mes+Año;
-                             }
-                         }   
-                     }else{
-                         System.out.println("Mes invalido");
-                         }
-                    if (dia>0 && dia>=1 && dia<=31) {
-                         System.out.println("La fecha ingresada es: "+dia+"/"+mes+"/"+año);
-                           String Dia = Integer.toString(dia);
-                           String Mes = Integer.toString(mes);
-                           String Año= Integer.toString(año);
-                           PFechaCo= Dia+Mes+Año;
-                          }else{
-                           System.out.println("Dia invalido");
-                          }
-                    
-                      
-                 } else{
-                   System.out.println("Año invalido");
-                     }
-         
-               System.out.println("Digite el Precio del libro");
-               PPrecio= sc2.nextInt();
-                int lugar = 0;
-                int sw=0;
-                 for (int j = 0; j < cont; j++) {
-                 if (libreria[j].equals(PTitulo)) {
-                     lugar=j;
-                     sw=1;
-                     break;
-                 }
-             }
-             if (sw==1) {
-                 inv.Ejemplares[lugar]=inv.Ejemplares[lugar]+PUnidInv;
-                 inv.Estado[lugar]= 1;
-             }else{
-             libreria[lugar]= PTitulo;
-             inv.Autor[lugar]=PNombre;
-             inv.Apellido[lugar]=PApellido;
-             inv.ISBN[lugar]= PISBN;
-             inv.Ejemplares[lugar]= PUnidInv;
-             inv.FechaCopyright[lugar]=PFechaCo;
-             inv.Precio[lugar]=PPrecio;
-             inv.Estado[lugar]=1;
-             cont+=1;
-             }*/
-            }
-             System.out.println("Desea ingresar otro libro?(S/N)");
-             Respu = sc2.next().toUpperCase();
-         }
-}
     
-public void llenarEspacios(){
-    for(i=0; i<libreria.length;i++){
-        libreria[i]="Z/A";
+    public String llenadodeLibros(){
+            cont = pos;
+    String Respu = "s";
+    while ("s".equals(Respu)) {
+        if (sc2.hasNextLine()) {
+            sc2.nextLine();
+        }
+        System.out.println("Digite el Titulo del libro");
+        PTitulo = sc2.nextLine();
+        System.out.println("Digite el Nombre del autor");
+        PNombre = sc2.next();
+        System.out.println("Digite el Apellido del autor");
+        PApellido = sc2.next();
+        System.out.println("Digite el ISBN");
+        PISBN = sc2.next();
+        System.out.println("Digite la Editorial");
+        PEditorial = sc2.next();
+        System.out.println("Digite la Cantidad de Copias");
+        PUnidInv = sc2.nextInt();
+        System.out.println("Digite la Fecha de Copiright");
+        System.out.println("Ingrese el dia");
+        int dia = sc2.nextInt();
+        System.out.println("Ingrese el mes");
+        int mes = sc2.nextInt();
+        System.out.println("Ingrese el año");
+        int año = sc2.nextInt();
+        if (año >= 1800 && año <= 2024) {
+            if (mes >= 1 && mes <= 12) {
+                if (mes == 2) {
+                    if (dia > 0 && dia >= 1 && dia <= 28) {
+                        System.out.println("La fecha ingresada es: " + dia + "/" + mes + "/" + año);
+                        String Dia = Integer.toString(dia);
+                        String Mes = Integer.toString(mes);
+                        String Año = Integer.toString(año);
+                        PFechaCo = Dia + Mes + Año;
+                    }
+                }
+            } else {
+                System.out.println("Mes invalido");
+            }
+            if (dia > 0 && dia >= 1 && dia <= 31) {
+                System.out.println("La fecha ingresada es: " + dia + "/" + mes + "/" + año);
+                String Dia = Integer.toString(dia);
+                String Mes = Integer.toString(mes);
+                String Año = Integer.toString(año);
+                PFechaCo = Dia + Mes + Año;
+            } else {
+                System.out.println("Dia invalido");
+            }
+        } else {
+            System.out.println("Año invalido");
+        }
+
+        System.out.println("Digite el Precio del libro");
+        PPrecio = sc2.nextInt();
+
+        int lugar = cont; // Asignar lugar después de verificar si el libro ya existe
+
+        int sw = 0;
+        for (int i = 0; i < cont; i++) {
+            if (inv.Titulo[i].equals(PTitulo)) {
+                lugar = i;
+                sw = 1;
+                break;
+            }
+        }
+        if (sw == 1) {
+            inv.Ejemplares[lugar] += PUnidInv;
+            inv.Estado[lugar] = 1;
+        } else {
+            inv.Titulo[lugar] = PTitulo;
+            inv.Autor[lugar] = PNombre;
+            inv.Apellido[lugar] = PApellido;
+            inv.ISBN[lugar] = PISBN;
+            inv.Ejemplares[lugar] = PUnidInv;
+            inv.FechaCopyright[lugar] = PFechaCo;
+            inv.Precio[lugar] = PPrecio;
+            inv.Estado[lugar] = 1;
+            inv.Editorial[lugar]=PEditorial;
+            cont += 1;
+        }
+        System.out.println("Desea ingresar otro libro?(S/N)");
+        Respu = sc2.next();
+    }
+    return null;
+}
+
+    
+    public void venderLibros(){
+      String Respuc = "s";
+    int PCantic;
+    while ("s".equals(Respuc)) {
+        if (sc2.hasNextLine()) {
+            sc2.nextLine();
+        }
+        System.out.println("Ingrese el titulo del libro");
+        PTitulo = sc2.nextLine();
+        System.out.println("Ingrese el nombre del autor");
+        PNombre = sc2.nextLine();
+        System.out.println("Ingrese el apellido del autor");
+        PApellido = sc2.nextLine();
+        System.out.println("Ingrese la cantidad de libros que desea comprar");
+        PCantic = sc2.nextInt();
+        
+        boolean libroEncontrado = false;
+        
+        for (int i = 0; i < 100; i++) {
+            if (inv.Autor[i] != null && inv.Apellido[i] != null && inv.Titulo[i] != null) {
+                if (inv.Autor[i].equals(PNombre) && inv.Apellido[i].equals(PApellido) && inv.Titulo[i].equals(PTitulo)) {
+                    libroEncontrado = true;
+                    if (inv.Ejemplares[i] >= PCantic) {
+                        inv.Ejemplares[i] -= PCantic;
+                        inv.Librosvendidos[i]= PCantic;
+                        if (inv.Ejemplares[i] == 0) {
+                            inv.Estado[i] = 0;
+                            System.out.println("Compra exitosa");
+                        }
+                    } else {
+                        System.out.println("Solo contamos con " + inv.Ejemplares[i] + " unidades para la venta");
+                        System.out.println("Desea comprarlas? (S/N)");
+                        String Resp = sc2.next();
+                        if ("S".equalsIgnoreCase(Resp)) {
+                            inv.Ejemplares[i] = 0;
+                            inv.Estado[i] = 0;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (!libroEncontrado) {
+            System.out.println("No se encuentra el libro deseado");
+        }
+
+        System.out.println("Desea comprar otro libro? (S/N)");
+        Respuc = sc2.next();
     }
 }
+   
+    public String Bibliografia(){
+        System.out.println("Hola");
+        String Resp="S";
+        while("S".equals(Resp)){
+            if (sc2.hasNextLine()) {
+        sc2.nextLine();
+    }
+             System.out.println("Ingrese el nombre del titulo ");
+              PTitulo=sc2.nextLine();
+           for (int i = 0; i < 100; i++) {
+            if (!"".equals(inv.Titulo[i])) {
+                 if (inv.Titulo[i].equals(PTitulo)) {
+                 System.out.println("El titulo es: "+ inv.Titulo[i]);
+                 System.out.println("El nombre del autor es: "+ inv.Autor[i]+ inv.Apellido[i]);
+                 System.out.println("El ISBN es: "+ inv.ISBN[i]);
+                 System.out.println("La fecha de copyright es: "+ inv.FechaCopyright[i]);
+                 break;
+                 }else{
+                     System.out.println("Libro no encontrado");
+                 }
+            }
+           }
+           System.out.println("Desea consultar otra bibliografia?(S/N)");
+           Resp=sc2.next();
+         } 
+        return Resp;
+    }
 
-public void bibliografiaLibro(){}
-public void listadoLibros(){}
-public void librosVendidos(){}
+    
+    public String Listadolibros(){
+                System.out.println("Los libros son:");
+    for (int i = 0; i < 100; i++) {
+        if (inv.Titulo[i] != null && !"".equals(inv.Titulo[i])) {
+            System.out.println(inv.Titulo[i]);
+        } else {
+            break;
         }
+    }
+    return null;
+}
